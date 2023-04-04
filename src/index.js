c2d47e69cfde8cf5299bc4f31355e3e936b63d66
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-04-04 16:16:25 +0200

    test catalog (KO Pinia)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 
 // import DropdownList from "../node_modules/luxembourg-geoportail/src/components/common/dropdown-list.vue";
 // import * as css from "../node_modules/luxembourg-geoportail/bundle/assets/style-85969ff6.css";
-import { DropdownList } from "../node_modules/luxembourg-geoportail/bundle/my-lib.mjs";
+import { DropdownList, Catalog } from "../node_modules/luxembourg-geoportail/bundle/my-lib.mjs";
 
 import { defineCustomElement } from 'vue';
 
 const DropdownListElement = defineCustomElement(DropdownList)
 customElements.define('dropdown-list', DropdownListElement)
+
+const CatalogElement = defineCustomElement(Catalog)
+customElements.define('catalog-tree', CatalogElement)
 // DropdownListElement.options = [{ label: "toto", value: "toto" }, { label: "toto1", value: "toto1" }];
 
 // console.log(DropdownListElement.options)
