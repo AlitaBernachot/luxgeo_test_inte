a96b87a986aa96dd6f116b2b8ee01aaf223561de
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-04-05 15:10:28 +0200

    test with new wrapper for web comp (OK)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@
 
 // import DropdownList from "../node_modules/luxembourg-geoportail/src/components/common/dropdown-list.vue";
 // import * as css from "../node_modules/luxembourg-geoportail/bundle/assets/style-85969ff6.css";
-import { DropdownList, Catalog } from "../node_modules/luxembourg-geoportail/bundle/my-lib.mjs";
+import { app, i18next, createElementInstance, createPinia, VueDOMPurifyHTML, backend, I18NextVue, DropdownList, CatalogTree } from "../node_modules/luxembourg-geoportail/bundle/my-lib.mjs";
+
+
+
+// i18next.use(backend)
+// i18next.init({
+//   lng: 'fr',
+//   debug: false,
+//   defaultNS: 'client',
+//   supportedLngs: ['de', 'en', 'fr', 'lb'],
+//   ns: ['client', 'legends', 'server', 'tooltips'],
+//   fallbackLng: 'fr',
+//   backend: {
+//     loadPath: `./assets/locales/{{ns}}.{{lng}}.json`, // ! use assets instead of ./assets because of theme path in url
+//   },
+// })
+
+// app.use(createPinia())
+// app.use(I18NextVue, { i18next })
+// app.use(VueDOMPurifyHTML)
+// app.mount('#app')
+
+
 
 import { defineCustomElement } from 'vue';
 
-const DropdownListElement = defineCustomElement(DropdownList)
+const DropdownListElement = createElementInstance(DropdownList)
 customElements.define('dropdown-list', DropdownListElement)
 
-const CatalogElement = defineCustomElement(Catalog)
+const CatalogElement = createElementInstance(CatalogTree)
 customElements.define('catalog-tree', CatalogElement)
-// DropdownListElement.options = [{ label: "toto", value: "toto" }, { label: "toto1", value: "toto1" }];
 
 // console.log(DropdownListElement.options)
 
